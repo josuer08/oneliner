955cd52c6f12cb03d5ce211c9ba77abc228343a2
Oscar J Rodriguez <josuer08@gmail.com>
2021-01-05 23:14:20 -0500

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+##This is just a random incomplete unimportant example of the actual "payload for the server"
+
+echo "************PRINT SOME TEXT***************\n"
+echo "Hello World!!!"
+echo "\n"
+echo "Resources:"
+echo "\n"
+echo "Addresses:"
+echo "$(ifconfig)"
+echo "\n"
